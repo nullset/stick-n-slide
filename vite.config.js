e24f9d65abce645eed716ef32e536554175f47c3
Nathan Wright <nathan@brandalism.com>
2022-09-06 11:13:57 +0300

    ensure that stick-n-slide exports only a single JS file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
+  plugins: [cssInjectedByJsPlugin()],
   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
     lib: {
       entry: resolve(__dirname, "index.js"),
       name: "Stick-n-Slide",
